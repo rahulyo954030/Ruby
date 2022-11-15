3eb768062936e5ccfd05b69b2a4db9f419affc85
Rahul Singh <100074739+rahulyo954030@users.noreply.github.com>
2022-11-15 18:54:37 +0530

    questions added

--- index.rb
+++ index.rb
@@ -1,1 +1,45 @@
+# // Ques 1 : Factorial of 5
+# // let num = 5
+# // const check = (num)=>{
+ 
+# //     if(num<=1){
+# //         return 1
+# //     }
+# //    return num* check(num-1)
+# // }
+# // console.log(check(num))
 
+# // function check(){
+
+# // }
+
+# //Ques 2: 3 3 3 = 8
+
+# //Ques 3 :  How many days, hours, minutes and seconds are there in 1,000,000 seconds
+
+
+
+
+# // Write the following programsare 
+# // 1. Find the odd and even numbers in the array
+# // 2. Find the vowels and consonants in a word/sentence
+# // 3. Reverse a string without using built in methods
+# // 4. Split the words in a sentence
+# // 5. Find the prime numbers in an array
+# // 6. Find the highest number in an array
+# // 7. Find the 3rd lowest number in an array
+# // 8. Generate Fibonacci series
+# // 9. Find the missing numbers in a series
+# // 10. Find the factorial of a number using recursion
+# // 11. Find if the word is a palindrome
+# // 12. Writ
+# // 12. Write a program to compare two strings
+# // 13. Sort the numbers/words in ascending/descending order
+# // 14. Understand and implement all the Searching (Binary, Linear, Jump), Sorting (Quick, Bubble, Merge, Insertion, Selection, heap) algorithms
+# // 15. Understand and implement Arrays, data types, and linked-lists
+# // 16. Implement as many programs as possible in the areas Fibonacci series, Factorial of a number using Recursion 
+# // 17. Palindrome, Matrix manipulations, Prime numbers
+# // 18. OOPS concepts (Abstraction, Encapsulation, Inheritance, Polymorphism) with examples
+# // 21. Collections classes, Static, Volatile, Synchronize, Serialize, Inner classes
+# // Praveen Gopinath12:39 PM
+# // https://www.tutorialspoint.com/ruby-on-rails/ruby-on-rails-tutorial.pdf
