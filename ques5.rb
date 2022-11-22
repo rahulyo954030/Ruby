f3905f05cfb9e6a7ec7b06a20502110398121241
Rahul Singh <100074739+rahulyo954030@users.noreply.github.com>
2022-11-22 23:39:20 +0530

    done ques5

--- ques5.rb
+++ ques5.rb
@@ -1,12 +1,28 @@
 # Find the prime numbers in an array
 
-var numArray = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+# Ruby program to find the prime numbers 
+# from the array
 
-numArray = numArray.filter((number) => {
-  for (var i = 2; i <= Math.sqrt(number); i++) {
-    if (number % i === 0) return false;
-  }
-  return true;
-});
+arr = [12,45,23,39,37];
 
-console.log(numArray);
+i = 0;
+j = 0;
+flag = 0;
+        
+puts "Prime Numbers are:"
+while(i<arr.size)
+    flag = 0;
+    j=2;
+    
+    while(j<arr[i]/2)
+        if(arr[i]%j==0)
+            flag=1;
+        end
+        j=j+1
+    end
+    
+    if(flag == 0)
+        print arr[i]," ";
+    end
+    i=i+1		
+end
